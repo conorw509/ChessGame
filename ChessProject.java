aeca8ec373d4ce6ea542ec06a1cea9a70f111e16
Conor Ward <conorward509@hotmail.com>
2018-10-10 11:58:40 +0100

    pawn

--- ChessProject.java
+++ ChessProject.java
@@ -1164,6 +1164,96 @@ public class ChessProject extends JFrame implements MouseListener, MouseMotionLi
             }
         }
 
+      /*  if(pieceName.equals("WhitePawn")){
+			if(startY == 1)
+			{
+				if((startX == (e.getX()/75))&&((((e.getY()/75)-startY)==1)||((e.getY()/75)-startY)==2))
+				{
+					if((((e.getY()/75)-startY)==2)){
+						if((!piecePresent(e.getX(), (e.getY())))&&(!piecePresent(e.getX(), (e.getY()+75)))){
+							validMove = true;					
+						}
+						else{
+							validMove = false;
+						}							
+					}
+					else{
+						if((!piecePresent(e.getX(), (e.getY()))))
+						{
+							validMove = true;					
+						}	
+						else{
+							validMove = false;
+						}													
+					}
+				}
+				else{
+					validMove = false;					
+				}
+			}
+			else{
+				int newY = e.getY()/75;
+				int newX = e.getX()/75;				
+				if((startX-1 >=0)||(startX +1 <=7))
+				{
+					if((piecePresent(e.getX(), (e.getY())))&&((((newX == (startX+1)&&(startX+1<=7)))||((newX == (startX-1))&&(startX-1 >=0)))))
+					{
+						if(checkWhiteOponent(e.getX(), e.getY())){
+							validMove = true;
+							if(startY == 6){
+								success = true;
+							}						
+						}
+						else{
+							validMove = false;
+						}
+					}
+					else{
+						if(!piecePresent(e.getX(), (e.getY()))){
+							if((startX == (e.getX()/75))&&((e.getY()/75)-startY)==1){
+								if(startY == 6){
+									success = true;
+								}
+								validMove = true;
+							}
+							else{
+								validMove = false;
+							}				
+						}
+						else{
+							validMove = false;	
+						}
+					}
+				}
+				else{
+					validMove = false;
+				}				
+			}			
+        }*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         if (!validMove) {
             int location = 0;
             if (startY == 0) {
